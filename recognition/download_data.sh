b4d4ae62dcae019c27710f2a41100a155baacd55
Anh DH <anhdhbn@gmail.com>
2019-10-02 00:40:58 +0700

    fix bash

--- recognition/download_data.sh
+++ recognition/download_data.sh
@@ -1,1 +1,7 @@
-[ -f /app/data/data.pkl ] && mkdir -p data && wget data.pkl -P /app/data/ http://download.recofat.vn/data.pkl
+if [ -e ./data/data.pkl ] 
+then
+    echo "data.pkl downloaded"
+else
+    echo "downloading data.pkl"
+    mkdir -p data && wget data.pkl -P ./data/ http://download.recofat.vn/data.pkl
+fi
--- recognition/download_models.sh
+++ recognition/download_models.sh
@@ -1,1 +1,7 @@
-[ -f /app/models/frozen_model.pb ] && mkdir -p models && wget frozen_model.pb -P /app/models/ http://download.recofat.vn/frozen_model.pb
+if [ -e ./models/frozen_model.pb ] 
+then
+    echo "frozen_model.pb downloaded"
+else
+    echo "downloading frozen_model.pb"
+    mkdir -p models && wget frozen_model.pb -P ./models/ http://download.recofat.vn/frozen_model.pb
+fi
