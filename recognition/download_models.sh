10594231ae5a8f174ec29d34a02057e7af12aa33
Anh DH <anhdhbn@gmail.com>
2019-10-03 19:34:10 +0700

    fix issue download models

--- recognition/download_models.sh
+++ recognition/download_models.sh
@@ -3,5 +3,7 @@ then
     echo "frozen_model.pb downloaded"
 else
     echo "downloading frozen_model.pb"
-    mkdir -p models && wget frozen_model.pb -P ./models/ http://download.recofat.vn/frozen_model.pb
+    mkdir -p models
+    wget frozen_model.zip -P ./models/ http://download.recofat.vn/frozen_model.zip
+    unzip ./models/frozen_model.zip -d ./models/
 fi
